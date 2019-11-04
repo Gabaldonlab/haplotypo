53da841ddcb9c85ab0282c3497c2213b03368b14
Manu <manu.molinam@gmail.com>
2019-11-04 10:28:38 +0100

    Update install.3.5.sh

--- scripts/install.3.5.sh
+++ scripts/install.3.5.sh
@@ -166,7 +166,7 @@ dep_folder=/home/dependencies
 PATH="$dep_folder/samtools-1.9/:${PATH}"
 PATH="$dep_folder/bcftools-1.9/:${PATH}"
 PATH="$dep_folder/bwa-0.7.15/:${PATH}"
-echo 'alias haplotypo="python $(pwd)/bin/haplotypo.py"' >> ~/.bashrc
+echo 'alias haplotypo="python $(pwd)/bin/3.5/haplotypo.py"' >> ~/.bashrc
 
 apt-get clean
 set -x; rm -rf /var/lib/apt/lists/*
