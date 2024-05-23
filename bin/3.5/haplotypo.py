1239f1aebb277b8fb401dce6d3c06110ec55daf5
redondrio <alvaroredondodelrio@gmail.com>
2024-05-23 13:52:31 +0200

    Added support for single-end reads

--- bin/3.5/haplotypo.py
+++ bin/3.5/haplotypo.py
@@ -2,9 +2,9 @@
 
 """
 Pipeline: Read mapping and variant calling on phased genomes with correction for variants and respective haplotype reconstruction
-12/02/2018
-Veronica Mixao 
-CRG (Barcelona)
+23/05/2024
+Veronica Mixao, Alvaro Redondo
+CRG (Barcelona) - BSC
 """
 
 import argparse
@@ -33,8 +33,9 @@ parser.add_argument("-hapA", "--hapA", dest="hapA", action= "store", help="Haplo
 parser.add_argument("-hapB", "--hapB", dest="hapB", action= "store", help="Haplotype B from reference genome (fasta). Warning: the name of each chromosome should be chr*_B")
 parser.add_argument("-idA", "--TagName_hapA", dest="tagName_hapA", action="store", default="hapA", help="Tag to be added in the output files relative to hapA [default: hapA]")
 parser.add_argument("-idB", "--TagName_hapB", dest="tagName_hapB", action="store", default="hapB", help="Tag to be added in the output files relative to hapB [default: hapB]")
-parser.add_argument("-f1", "--fastq1", dest="fastq1", default="None", help="Illumina paired-end reads 1 (only required for read mapping)")
-parser.add_argument("-f2", "--fastq2", dest="fastq2", default="None", help="Illumina paired-end reads 2 (only required for read mapping)")
+parser.add_argument("-f1", "--fastq1", dest="fastq1", default="None", help="Illumina paired-end reads 1 / Single-end reads (only required for read mapping)")
+parser.add_argument("-f2", "--fastq2", dest="fastq2", default="None", help="Illumina paired-end reads 2")
+parser.add_argument("-s", "--single_end", dest="single_end", action="store_true", help="Flag for single-end reads")
 parser.add_argument("-bA", "--bamA", dest="bamfileA", default="None", action="store", help="Bam file using haplotype A as reference")
 parser.add_argument("-bB", "--bamB", dest="bamfileB", default="None", action="store", help="Bam file using haplotype B as reference")
 parser.add_argument("-caller", "--variant_caller", dest="caller", default="GATK", help="Program for variant calling (GATK / bcftools / freebayes) [default: GATK]")
@@ -74,15 +75,65 @@ Please cite: Pegueroles C. et. al. Bioinformatics 36(8)2569-2571\n\n"""
 ######################
 #### All pipeline ####
 ######################
-if fastq1 != "None" and fastq2 != "None" and bamA == "None" and bamB == "None":
-	os.system(python_path+" " + haplotypo + "mapping.py -o " + outDir + " -thr " + numberthr + " -hapA " + hapA + " -hapB " + hapB + " -idA " + tagName_hapA  + " -idB " + tagName_hapB + " -f1 " + fastq1 + " -f2 " + fastq2)
-	os.system(python_path+" " + haplotypo + "var_calling.py -o " + outDir + " -thr " + numberthr + " -hapA " + hapA + " -hapB " + hapB + " -idA " + tagName_hapA + " -idB " + tagName_hapB + " -bA " + outDir + "/" + tagName_hapA + ".mkd.bam " + " -bB " + outDir + "/" + tagName_hapB + ".mkd.bam " + " -c " + coverage + " -caller " + caller + " -p 2")
-	if coord == "None":
-		os.system(python_path+" " + haplotypo + "VCFcorr_alleles.py -A " + outDir + "/" + tagName_hapA + ".pass.snp.vcf" + " -B " + outDir + "/" + tagName_hapB + ".pass.snp.vcf" + " -fastaA " + hapA + " -fastaB " + hapB + " -cA " + outDir + "/" + tagName_hapA + ".corrected" + "_amb" + amb + ".vcf" + " -cB " + outDir + "/" + tagName_hapB + ".corrected" + "_amb" + amb + ".vcf" + " -amb " + amb) 
-	else:
-		os.system(python_path+" " + haplotypo + "VCFcorr_alleles.py -A " + outDir + "/" + tagName_hapA + ".pass.snp.vcf" + " -B " + outDir + "/" + tagName_hapB + ".pass.snp.vcf" + " -fastaA " + hapA + " -fastaB " + hapB + " -cA " + outDir + "/" + tagName_hapA + ".corrected" + "_amb" + amb + ".vcf" + " -cB " + outDir + "/" + tagName_hapB + ".corrected" + "_amb" + amb + ".vcf" + " -amb " + amb + " -coor " + coord) 
-	os.system(python_path+" " + haplotypo + "haplomaker.py -o " + outDir + " -hapA " + hapA + " -hapB " + hapB + " -corrA " + outDir + "/" + tagName_hapA + ".corrected" + "_amb" + amb + ".vcf" + " -corrB " + outDir + "/" + tagName_hapB + ".corrected" + "_amb" + amb + ".vcf") 
+if not args.single_end and fastq2 == "None":
+	parser.error("ERROR: Reverse reads not found, use the -s flag for single-end data")
 
+if fastq1 != "None" and bamA == "None" and bamB == "None":
+	if fastq2 == "None" and args.single_end:
+		os.system(
+			f"{python_path} {haplotypo}mapping.py "
+			f"-o {outDir} "
+			f"-thr {numberthr} "
+			f"-hapA {hapA} -hapB {hapB} "
+			f"-idA {tagName_hapA} -idB {tagName_hapB} "
+			f"-f1 {fastq1} -s"
+		)
+	else:
+		os.system(
+			f"{python_path} {haplotypo}mapping.py "
+			f"-o {outDir} "
+			f"-thr {numberthr} "
+			f"-hapA {hapA} -hapB {hapB} "
+			f"-idA {tagName_hapA} -idB {tagName_hapB} "
+			f"-f1 {fastq1} -f2 {fastq2}"
+		)
+	os.system(
+		f"{python_path} {haplotypo}var_calling.py "
+		f"-o {outDir} "
+		f"-thr {numberthr} "
+		f"-hapA {hapA} -hapB {hapB} "
+		f"-idA {tagName_hapA} -idB {tagName_hapB} "
+		f"-bA {outDir}/{tagName_hapA}.mkd.bam "
+		f"-bB {outDir}/{tagName_hapB}.mkd.bam "
+		f"-c {coverage} -caller {caller} -p 2"
+	)
+	if coord == "None":
+		os.system(
+			f"{python_path} {haplotypo}VCFcorr_alleles.py "
+			f"-A {outDir}/{tagName_hapA}.pass.snp.vcf "
+			f"-B {outDir}/{tagName_hapB}.pass.snp.vcf "
+			f"-fastaA {hapA} -fastaB {hapB} "
+			f"-cA {outDir}/{tagName_hapA}.corrected_amb{amb}.vcf "
+			f"-cB {outDir}/{tagName_hapB}.corrected_amb{amb}.vcf "
+			f"-amb {amb}"
+		)
+	else:
+		os.system(
+			f"{python_path} {haplotypo}VCFcorr_alleles.py "
+			f"-A {outDir}/{tagName_hapA}.pass.snp.vcf "
+			f"-B {outDir}/{tagName_hapB}.pass.snp.vcf "
+			f"-fastaA {hapA} -fastaB {hapB} "
+			f"-cA {outDir}/{tagName_hapA}.corrected_amb{amb}.vcf "
+			f"-cB {outDir}/{tagName_hapB}.corrected_amb{amb}.vcf "
+			f"-amb {amb} -coor {coord}"
+		)
+	os.system(
+		f"{python_path} {haplotypo}haplomaker.py "
+		f"-o {outDir} "
+		f"-hapA {hapA} -hapB {hapB} "
+		f"-corrA {outDir}/{tagName_hapA}.corrected_amb{amb}.vcf "
+		f"-corrB {outDir}/{tagName_hapB}.corrected_amb{amb}.vcf"
+	)
 
 ######################################
 #### Only from variant calling on ####
--- bin/3.5/mapping.py
+++ bin/3.5/mapping.py
@@ -2,9 +2,9 @@
 
 """
 Pipeline: Read mapping on phased or unphased genomes
-12/02/2019
-Veronica Mixao, Laia Carrete
-CRG (Barcelona)
+23/05/2024
+Veronica Mixao, Laia Carrete, Alvaro Redondo
+CRG (Barcelona) - BSC
 """
 
 import argparse
@@ -30,8 +30,9 @@ parser.add_argument("-ref", "--reference", dest="ref", action= "store", default=
 parser.add_argument("-idA", "--TagName_hapA", dest="tagName_hapA", action="store", default="hapA", help="Tag to be added in the output files relative to hapA [default: hapA]")
 parser.add_argument("-idB", "--TagName_hapB", dest="tagName_hapB", action="store", default="hapB", help="Tag to be added in the output files relative to hapB [default: hapB]")
 parser.add_argument("-t", "--TagName", dest="tagName", action="store", default="strain", help="Tag to be added in the output files if using a non-phased reference [default: strain] - Only required if no phased haplotypes are provided")
-parser.add_argument("-f1", "--fastq1", dest="fastq1", required=True, help="Illumina paired-end reads 1")
-parser.add_argument("-f2", "--fastq2", dest="fastq2", required=True, help="Illumina paired-end reads 2")
+parser.add_argument("-f1", "--fastq1", dest="fastq1", required=True, help="Illumina paired-end reads 1 / Single-end reads")
+parser.add_argument("-f2", "--fastq2", dest="fastq2", default="None", help="Illumina paired-end reads 2")
+parser.add_argument("-s", "--single_end", dest="single_end", action="store_true", help="Flag for single-end reads")
 
 args = parser.parse_args()
 
@@ -56,38 +57,71 @@ numberthr = str(args.threads)
 #### Read Mapping ####
 ######################
 
-def mapping(reference, f1, f2, outdir, tag, threads):
+def mapping(reference, single_end, outdir, tag, threads, f1, f2 = "None"):
 	#Index the reference
 	print("Indexing the reference...")
-	cmd_indexFasta = bwa + " index " + reference
+	cmd_indexFasta = f"{bwa} index {reference}"
 	print(cmd_indexFasta)
 	os.system(cmd_indexFasta)
 	
 	#BWA-MEM
 	print("\n", "Read mapping...")
-	cmdbwa = bwa + ' mem -R "@RG\\tID:' + tag + '\\tSM:' + tag + '" -t ' + threads + " " + reference + " " + f1 + " " + f2 + " > " + outdir + "/" + tag + ".sam"
+	if single_end:
+		print("Mapping single-end reads")
+		cmdbwa = (
+    		f"{bwa} mem "
+			f"-R \"@RG\\tID:{tag}\\tSM:{tag}\" -t {threads} "
+    		f"{reference} {f1} "
+			f"> {outdir}/{tag}.sam"
+		)
+	else:
+		print("Mapping paired-end reads")
+		cmdbwa =(
+    		f"{bwa} mem "
+			f"-R \"@RG\\tID:{tag}\\tSM:{tag}\" -t {threads} "
+    		f"{reference} {f1} {f2} "
+    		f"> {outdir}/{tag}.sam"
+		)
 	print(cmdbwa)
 	os.system(cmdbwa)
 	print("BWA MEM done.")
 	
 	#PICARD
 	print("\n", "Sorting...")
-	cmd_toBAM_sorted = picard + " SortSam " + "-I " + outdir + "/" + tag + ".sam " + "-O " + outdir + "/" + tag + ".bam" + " -SO coordinate" 
+	cmd_toBAM_sorted = (
+		f"{picard} SortSam "
+		f"-I {outdir}/{tag}.sam "
+		f"-O {outdir}/{tag}.bam "
+		f"-SO coordinate "
+	)
 	print(cmd_toBAM_sorted)
 	os.system(cmd_toBAM_sorted)
 	
 	print("\n", "Mark duplicates...")
-	cmd_toBAM_mkd = picard + " MarkDuplicates " + "-I " + outdir + "/" + tag + ".bam " + "-O " + outdir + "/" + tag + ".mkd.bam" + " -M " + outdir + "/" + tag + ".mkd.metric.txt" 
+	cmd_toBAM_mkd = (
+		f"{picard} MarkDuplicates "
+		f"-I {outdir}/{tag}.bam "
+		f"-O {outdir}/{tag}.mkd.bam "
+		f"-M {outdir}/{tag}.mkd.metric.txt"
+	)
 	print(cmd_toBAM_mkd)
 	os.system(cmd_toBAM_mkd)
 	
 	print("\n", "Indexing...")
-	cmd_indexBAM = picard + " BuildBamIndex " + "-I " + outdir + "/" + tag + ".mkd.bam "
+	cmd_indexBAM = (
+		f"{picard} BuildBamIndex "
+		f"-I {outdir}/{tag}.mkd.bam"
+	)
 	print(cmd_indexBAM)
 	os.system(cmd_indexBAM)
 	
 	print("Checking mapping quality...")
-	cmd_qual = picard + " CollectAlignmentSummaryMetrics -R " + reference + " -I " + outdir + "/" + tag + ".mkd.bam " + " -O " + outdir + "/" + tag + ".mkd.stat.txt "
+	cmd_qual = (
+		f"{picard} CollectAlignmentSummaryMetrics "
+		f"-R {reference} "
+		f"-I {outdir}/{tag}.mkd.bam "
+		f"-O {outdir}/{tag}.mkd.stat.txt"
+	)
 	print(cmd_qual)
 	os.system(cmd_qual)
 	
@@ -102,20 +136,24 @@ def mapping(reference, f1, f2, outdir, tag, threads):
 #### Running ####
 #################
 
+if not args.single_end and fastq2 == "None":
+	parser.error("ERROR: Reverse reads not found, use the -s flag for single-end data")
+
 if ref == "None" and hapA != "None" and hapB != "None":	
 	print("Read mapping will be performed on phased haplotypes...")
 	print("*********************************************************************")
 	print("Read mapping against haplotype A")
-	mapping(hapA, fastq1, fastq2, outDir, tagName_hapA, numberthr)
+	mapping(hapA, args.single_end, outDir, tagName_hapA, numberthr, fastq1, fastq2)
 	print("*********************************************************************")
 	print("*********************************************************************")
 	print("Read mapping against haplotype B")
-	mapping(hapB, fastq1, fastq2, outDir, tagName_hapB, numberthr)
+	mapping(hapB, args.single_end, outDir, tagName_hapB, numberthr, fastq1, fastq2)
 	print("*********************************************************************")
 	
 elif ref != "None" and hapA == "None" and hapB == "None":
-	 print("Read mapping will be performed...")
-	 mapping(ref, fastq1, fastq2, outDir, tagName, numberthr)
+	print("Read mapping will be performed...")
+	mapping(ref, args.single_end, outDir, tagName, numberthr, fastq1, fastq2)
+
 	
 else:
 	print("ERROR: Please revise the reference option you have given!")
